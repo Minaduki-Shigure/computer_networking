ecc12d085e8e6cf678a5a438f3d0338acfc953ff
Minaduki Shigure <171180571@smail.nju.edu.cn>
2020-05-09 02:26:15 +0800

    Node 3 will now be constructed properly.

--- assignments/RIPRouterAlgorithm/src/node3.c
+++ assignments/RIPRouterAlgorithm/src/node3.c
@@ -7,11 +7,11 @@ node_class Node3;
 
 void rtinit3(void)
 {
-    node_constructor(&Node3, 0);
+    node_constructor(&Node3, 3);
     rtinit(&Node3, initcost3, neighbour3);
 }
 
-void rtupdate3(struct rtpkt *rcvdpkt)
+void rtupdate3(struct rtpkt* rcvdpkt)
 {
     rtupdate(&Node3, rcvdpkt);
 }
